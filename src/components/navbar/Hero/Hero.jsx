7e9b22a812658ce6de2782c351db664606fe7b8c
subekshathing <shuvechchhathing@gmail.com>
2024-11-07 18:46:55 +0545

    navbar update

--- src/components/navbar/Hero/Hero.jsx
+++ src/components/navbar/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
         <span>Im alex, frontend develepor based in usa</span>
@@ -16,7 +17,12 @@ const Hero = () => {
         dignissimos maiores.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">connect with me</div>
+        <div className="hero-connect">
+          {" "}
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">my resume</div>
       </div>
     </div>
